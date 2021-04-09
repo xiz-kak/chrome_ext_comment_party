1904c1bac7bee8af44e56815ab554351e9456d1c
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2021-04-10 01:29:33 +0900

    add log

--- src/background.js
+++ src/background.js
@@ -39,6 +39,7 @@ function setupPusher(channelName) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       console.log(tabs);
       if (tabs && tabs.length > 0) {
+        console.log(tabs[0]);
         chrome.tabs.sendMessage(tabs[0].id, data, function(response) {});
       }
     });
