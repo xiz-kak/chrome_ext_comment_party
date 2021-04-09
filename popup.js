123966e9260c0053423c2e2009ea7ef9ffac7316
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2021-04-10 01:08:01 +0900

    add basic logic

---
+++ popup.js
@@ -0,0 +1,33 @@
+let btnComment = document.getElementById("comment");
+btnComment.addEventListener("click", async () => {
+  const data = {
+    to: "contentScript",
+    action: "messageSent",
+    value: "haro~"
+  };
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, data, function(response) {});
+  });
+});
+
+
+let btnCon= document.getElementById("connect");
+btnCon.addEventListener("click", async () => {
+  const data = {
+    to: "background",
+    action: "pusherConnect",
+    value: "my-channel"
+  };
+  chrome.runtime.sendMessage(data, function(response) {});
+});
+
+let btnDiscon = document.getElementById("disconnect");
+btnDiscon.addEventListener("click", async () => {
+  const data = {
+    to: "background",
+    action: "pusherDisconnect",
+    value: ""
+  };
+  chrome.runtime.sendMessage(data, function(response) {});
+});
+
